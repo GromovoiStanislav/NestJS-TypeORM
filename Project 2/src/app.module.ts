0060bb60d8f6240da923b61f7924104e4bd687ca
Gromovoi Stanislav <gromila12@rambler.ru>
2024-04-26 16:08:21 +0600

    Project 2

---
+++ Project 2/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from "@nestjs/common";
+import { UsersModule } from "./users/users.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.PG_HOST,
+      port: parseInt(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      entities: [__dirname + "/**/*.entity{.ts,.js}"],
+      synchronize: true
+    })
+  ],
+  controllers: [],
+  providers: []
+})
+export class AppModule {
+}
